e29131128bbe030228c575cc7429b7ffcc52c9ba
gouri1694 <igouri8@gmail.com>
2025-10-25 14:13:28 +0530

    Initial project setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US_Visa_Approval",
+    version="0.0.0",
+    author="Gouri Patil",
+    author_email="igouri8@gmail.com",
+    packages=find_packages()
+)
